477cbcd55971e32d41005dc95a4909141a51030b
Yusuke Shimizu <yshimizu@karahiyo.dev>
2023-03-08 14:53:21 +0900

    use Product Fetch API

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from '@forge/ui';
+import api, { route } from '@forge/api';
 
 const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
+  console.log(`Number of comments on this issue: ${comments.length}`);
+
   return (
     <Fragment>
       <Text>Hello world!!!</Text>
@@ -13,3 +18,9 @@ export const run = render(
     <App />
   </IssuePanel>
 );
+
+const fetchCommentsForIssue = async (issueIdOrKey) => {
+  const res = await api.asUser().requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`);
+  const data = await res.json();
+  return data.comments;
+}
